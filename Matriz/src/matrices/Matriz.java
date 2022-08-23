b91479b8c320eeae20c6d391600620f0565072ab
aula11 <aula11@aula11-06.aulas.unla.edu.ar>
2022-08-23 12:24:48 -0300

    update lo visto en clase

--- Matriz/src/matrices/Matriz.java
+++ Matriz/src/matrices/Matriz.java
@@ -1,31 +1,49 @@
 package matrices;
 
 public class Matriz {
-	private double[][] matriz1 = new double[3][3];
-	
+	private double[][] matriz1;
+
 	public Matriz(double[][] matriz1) {
 		this.matriz1 = matriz1;
 	}
-	
+
 	public void mostrarMatriz() {
+		mostrarMatriz(matriz1);
+	}
+
+	public void mostrarMatriz(double[][] matriz1) {
+
 		for (int i = 0; i < matriz1.length; i++) {
 			for (int j = 0; j < matriz1[i].length; j++) {
+				System.out.print("[");
+
 				System.out.print(matriz1[i][j]);
+				System.out.print("]");
+
 			}
 			System.out.print("\n");
 		}
 	}
-	
-	public double[][]sumar(double[][] matrizB){
-		double [][] suma = new double[matriz1.length][matriz1[0].length];
+
+	public double[][] sumar(double[][] matrizB) {
+		double[][] suma = new double[matriz1.length][matriz1[0].length];
 		for (int i = 0; i < matriz1.length; i++) {
-		    for (int j = 0; j < matriz1[i].length; j++) {
-		        suma[i][j] = matriz1[i][j] + matrizB[i][j];
-		    }
-		} 
+			for (int j = 0; j < matriz1[i].length; j++) {
+				suma[i][j] = matriz1[i][j] + matrizB[i][j];
+			}
+		}
 		return suma;
 	}
-	
+	public double[][] restar(double[][] matrizB) {
+		double[][] resta = new double[matriz1.length][matriz1[0].length];
+		for (int i = 0; i < matriz1.length; i++) {
+			for (int j = 0; j < matriz1[i].length; j++) {
+				resta[i][j] = matriz1[i][j] - matrizB[i][j];
+			}
+		}
+		return resta;
+	}
+
 	public double[][] getMatrizBidimensional() {
 		return matriz1;
 	}
@@ -33,5 +51,5 @@ public class Matriz {
 	public void setMatrizBidimensional(double[][] matriz1) {
 		this.matriz1 = matriz1;
 	}
-	
+
 }
